486c873bd80a3ee919dc7f9b14d488dddbabd9c4
ShintaroFuji <139973307+ShintaroFuji@users.noreply.github.com>
2023-08-06 15:57:38 +0900

    Add files via upload

---
+++ Web.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+#①スクレイピング対象のURLを指定
+url = "https://www.python.org/"
+
+#②URLにアクセスして、その内容を取得
+response = requests.get(url)
+
+#③BeautifulSoupを使って、取得したHTMLを解析
+soup = BeautifulSoup(response.text, 'html.parser')#html.parser＝解析器
+
+# <h1>と<p>タグのテキスト部分を取得
+print(soup.select("h1"))
+ 
+print(soup.select("p"))
